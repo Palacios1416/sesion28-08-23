ebf46be1110233dd568949c78b7d501a9d8b98d3
Dereck Palacios <dpalaciosurbi@gmail.com>
2023-08-28 17:14:18 -0600

    Ejemplo de condicionales simples y dobles

---
+++ Condicionales/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int edad = 10;
+        char sex = 'm';
+        if ((edad == 5 || sex = 'f') || (edad == 10 || sex == 'm')) {
+            System.out.println(" linea 1 dentro de if" + edad);
+            System.out.println(" liena 2 dentro de if" + sex);
+        } else {
+            System.out.println("Dentro del else");
+        }
+            
+        
+        
+        /*
+         * operadores relacionales 
+         * <, >, <=, >= , ==, != 
+         */
+    }
+}
